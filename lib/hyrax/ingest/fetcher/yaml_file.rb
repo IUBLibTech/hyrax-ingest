55aede6738b2e6c60092a47ef22a8813a51aa7c0
Daniel Pierce <dlpierce@indiana.edu>
2017-09-01 17:12:56 -0400

    Fetcher::YAMLFile

---
+++ lib/hyrax/ingest/fetcher/yaml_file.rb
@@ -0,0 +1,50 @@
+require 'hyrax/ingest/fetcher/base'
+require 'yaml'
+
+module Hyrax
+  module Ingest
+    module Fetcher
+      class YAMLFile < Base
+        attr_reader :filename, :yaml_path
+
+        def initialize(sip, options={})
+          raise ArgumentError, "Required option :filename is missing" unless options.key?(:filename)
+          raise ArgumentError, "Required option :yaml_path is missing" unless options.key?(:yaml_path)
+          @filename = options[:filename]
+          @yaml_path = options[:yaml_path]
+          super(sip)
+        end
+
+        def fetch
+          # TODO: log a warning in the event of empty results.
+          yaml.dig(*yaml_path)
+        end
+
+        private
+
+        # @return Boolean True if the first and last characters of the
+        #  @filename attribute are a slash, indicating it should be
+        #  interpreted as a regular expression.
+        def filename_is_regex?
+          filename.to_s[0] == '/' && filename.to_s[-1] == '/'
+        end
+
+        # @return
+        def yaml
+          @yaml ||= begin
+            file = if filename_is_regex?
+                     regexp = Regexp.new(filename[1..-2])
+                     sip.files.find { |file| File.basename(file) =~ regexp }
+                   else
+                     sip.files.find { |file| File.basename(file) == filename }
+                   end
+            raise Hyrax::Ingest::Errors::FileNotFoundInSIP.new(filename) unless file
+            YAML.load_file file
+          end
+          file.rewind if file
+          @yaml
+        end
+      end
+    end
+  end
+end
---
+++ spec/hyrax/ingest/fetcher/yaml_file_spec.rb
@@ -0,0 +1,47 @@
+require 'hyrax_helper'
+require 'hyrax/ingest/fetcher/yaml_file'
+
+
+RSpec.describe Hyrax::Ingest::Fetcher::YAMLFile do
+  let(:sip) { Hyrax::Ingest::SIP.new(path: "#{fixture_path}/sip_examples/ingest_work_from_yaml") }
+
+  describe '#fetch' do
+    context 'when :filename option is a string (not a regex) and yaml_path points to a value in the yaml' do
+      let(:options) { {filename: 'wells.yaml', yaml_path: %w[work title] } }
+
+      let(:fetcher_1) { described_class.new(sip, options) }
+      let(:fetcher_2) { described_class.new(sip, options) }
+
+      it 'returns the value from the YAML pointed to by the yaml_path' do
+        expect(fetcher_1.fetch).to eq 'Wells Documentary, Vision of Herman B Wells'
+      end
+
+      it 'can fetch same metadata from the same SIP using 2 different fetcher objects' do
+        expect(fetcher_1.fetch).to eq fetcher_2.fetch
+      end
+    end
+
+    context 'when :filename options is a regexp' do
+      let(:options) { { filename: '/_no_title.yaml$/', yaml_path: %w[work original_format] } }
+      let(:fetcher) { described_class.new(sip, options) }
+
+      it 'finds the file within the sip' do
+        expect(fetcher.fetch).to eq 'Open Reel Audio Tape'
+      end
+    end
+  end
+
+  describe '.new' do
+    context 'when missing :filename keyword argument' do
+      it 'raises an ArgumentError' do
+        expect { described_class.new(sip, yaml_path: 'foo') }.to raise_error ArgumentError
+      end
+    end
+
+    context 'when missing :yaml_path keyword argument' do
+      it 'raises an ArgumentError' do
+        expect { described_class.new(sip, filename: 'boo') }.to raise_error ArgumentError
+      end
+    end
+  end
+end
